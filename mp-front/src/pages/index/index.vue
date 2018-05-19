2af5fa0ae245a0905f2b3b58ac6fbaa681b0c994
张浩然 <zhanghaoran@MacBook-Pro-2.local>
2018-05-19 10:22:45 +0800

    增加了mpvue小程序的基础框架

---
+++ mp-front/src/pages/index/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    我是首页
+    <button @click="showToast">
+      点击提示
+    </button>
+    <mptoast />
+  </div>
+</template>
+
+<script>
+import mptoast from "mptoast";
+export default {
+  components: {
+    mptoast
+  },
+  methods: {
+    showToast() {
+      console.log(111);
+      this.$mptoast("我是提示信息");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.userinfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.userinfo-avatar {
+  width: 128rpx;
+  height: 128rpx;
+  margin: 20rpx;
+  border-radius: 50%;
+}
+
+.userinfo-nickname {
+  color: #aaa;
+}
+
+.usermotto {
+  margin-top: 150px;
+}
+
+.form-control {
+  display: block;
+  padding: 0 12px;
+  margin-bottom: 5px;
+  border: 1px solid #ccc;
+}
+
+.counter {
+  display: inline-block;
+  margin: 10px auto;
+  padding: 5px 10px;
+  color: blue;
+  border: 1px solid blue;
+}
+</style>
