bd89ca83abb64a61c25277ce9059ba8c546176a8
黄荣卫 <rongwei.huang@phicomm.com>
2018-05-27 18:14:18 +0800

    增加积分兑换接口

---
+++ backend/src/main/java/com/auts/mp/thereis/controller/mercenter/GoodsController.java
@@ -0,0 +1,12 @@
+package com.auts.mp.thereis.controller.mercenter;
+
+import com.auts.mp.thereis.controller.SBaseController;
+
+/**
+ * 积分兑换接口.
+ * @author huangrongwei
+ *
+ */
+public class GoodsController extends SBaseController {
+
+}
